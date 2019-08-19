e792919e4755d1b29808264f66eca8bd11f9999a
bhavinddarji <36255576+bhavinddarji@users.noreply.github.com>
2019-08-19 15:21:32 +0530

    Upload Source
    Upload Source

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeesList;
+        public MockEmployeeRepository()
+        {
+            _employeesList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name="Bhavin", Email="bhavin@solvis.technology",Department="IT" },
+                new Employee(){Id = 2, Name="Brijesh", Email="bhavin@solvis.technology",Department="IT" },
+                new Employee(){Id = 3, Name="Rajesh", Email="bhavin@solvis.technology",Department="HR" },
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeesList.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            //app.UseDefaultFiles(defaultFilesOptions);
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseFileServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hosting Environment :" + env.EnvironmentName);
+            });
+        }
+    }
+}
